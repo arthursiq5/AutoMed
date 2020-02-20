2ce45e4405d4dcd5eb2c75f1ffb234674daedc61
Arthur Siqueira <arthur.siq5@gmail.com>
2020-02-20 20:35:36 -0300

    criada janela principal

--- src/automed/AutoMed.java
+++ src/automed/AutoMed.java
@@ -5,6 +5,8 @@
  */
 package automed;
 
+import tela.JanelaPrincipal;
+
 /**
  *
  * @author arthur.siqueira
@@ -15,7 +17,8 @@ public class AutoMed {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JanelaPrincipal janela = new JanelaPrincipal();
+        janela.setVisible(true);
     }
     
 }
