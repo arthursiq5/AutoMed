1b0784be5105658b7b878d0fbd8ff349cd33b8d8
Arthur Siqueira <arthur.siq5@gmail.com>
2020-02-20 22:03:36 -0300

    conectando sistema no banco

--- src/automed/AutoMed.java
+++ src/automed/AutoMed.java
@@ -5,6 +5,9 @@
  */
 package automed;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 import tela.JanelaPrincipal;
 
 /**
@@ -12,13 +15,44 @@ import tela.JanelaPrincipal;
  * @author arthur.siqueira
  */
 public class AutoMed {
+    
+    public static Connection conexao = null;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JanelaPrincipal janela = new JanelaPrincipal();
-        janela.setVisible(true);
+        if(abrirConexao()){
+            JanelaPrincipal janela = new JanelaPrincipal();
+            janela.setVisible(true);
+        }else{
+            JOptionPane.showMessageDialog(null, "erro ao conectar no banco");
+        }
     }
     
+    private static boolean abrirConexao(){
+        try {
+            String dbdriver = "org.postgresql.Driver";
+            String dburl = "jdbc:postgresql://localhost:5432/automed";
+            String dbuser = "postgres";
+            String dbsenha = "postgres";
+
+            // Carrega Driver do Banco de Dados
+            Class.forName(dbdriver);
+
+            if (dbuser.length() != 0) // conexão COM usuário e senha
+            {
+                conexao = DriverManager.getConnection(dburl, dbuser, dbsenha);
+            } else // conexão SEM usuário e senha
+            {
+                conexao = DriverManager.getConnection(dburl);
+            }
+
+            return true;
+
+        } catch (Exception e) {
+            System.err.println("Erro ao tentar conectar: " + e);
+            return false;
+        }
+    }
 }
