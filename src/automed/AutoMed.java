21278f2de31af95e458260860fb3d206c7573128
Arthur Siqueira <arthur.siq5@gmail.com>
2020-02-27 21:50:00 -0300

    removido código não utilizado

--- src/automed/AutoMed.java
+++ src/automed/AutoMed.java
@@ -16,43 +16,15 @@ import tela.JanelaPrincipal;
  */
 public class AutoMed {
     
-    public static Connection conexao = null;
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if(abrirConexao()){
+        if(ConexaoBD.getInstance() != null){
             JanelaPrincipal janela = new JanelaPrincipal();
             janela.setVisible(true);
         }else{
             JOptionPane.showMessageDialog(null, "erro ao conectar no banco");
-        }
-    }
-    
-    private static boolean abrirConexao(){
-        try {
-            String dbdriver = "org.postgresql.Driver";
-            String dburl = "jdbc:postgresql://localhost:5432/automed";
-            String dbuser = "postgres";
-            String dbsenha = "postgres";
-
-            // Carrega Driver do Banco de Dados
-            Class.forName(dbdriver);
-
-            if (dbuser.length() != 0) // conexão COM usuário e senha
-            {
-                conexao = DriverManager.getConnection(dburl, dbuser, dbsenha);
-            } else // conexão SEM usuário e senha
-            {
-                conexao = DriverManager.getConnection(dburl);
-            }
-
-            return true;
-
-        } catch (Exception e) {
-            System.err.println("Erro ao tentar conectar: " + e);
-            return false;
         }
     }
 }
